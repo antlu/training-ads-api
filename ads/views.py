5b910a4ff95c5321e288f3c19f4d9c1401e848a8
Anton <web-gambit@mail.ru>
2020-08-31 21:43:39 +0500

    Return id & status code on ads creation

--- ads/views.py
+++ ads/views.py
@@ -1,11 +1,12 @@
 from rest_framework import filters, generics
+from rest_framework.response import Response
 
 from ads.models import Advertisement
 from ads.serializers import AdSerializer
 
 
 class AdList(generics.ListCreateAPIView):
-    """A list view for ads."""
+    """A list view for adding and viewing ads."""
 
     queryset = Advertisement.objects.all()
     serializer_class = AdSerializer
@@ -13,9 +14,17 @@ class AdList(generics.ListCreateAPIView):
     ordering_fields = ('price', 'created_at')
     ordering = '-created_at'
 
+    def post(self, *args, **kwargs):
+        """Return a customized response with the id of the ad."""  # noqa: DAR101,E501
+        response = super().post(*args, **kwargs)
+        return Response(
+            {'id': response.data.serializer.instance.id},
+            status=response.status_code,
+        )
+
 
 class AdDetails(generics.RetrieveAPIView):
-    """A detail view for an ad."""
+    """A detail view for viewing an ad."""
 
     queryset = Advertisement.objects.all()
     serializer_class = AdSerializer
